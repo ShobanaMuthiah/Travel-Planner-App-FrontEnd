83e26d6a09e0e7cf2b16a2f2e4c5655bafb30bed
ShobanaMuthiah <shobanam222@gmail.com>
2024-07-02 00:07:22 -1200

    updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,16 +8,16 @@ const Footer = () => {
     <footer className="bg-gray-400 text-white py-8">
       <div className="container mx-auto items-center text-center">
         <div className="text-center md:text-left">
-          <h5 className="text-lg font-semibold mb-2">Travel Planner</h5>
-          <p className="mb-2">Plan your trips with ease and joy.</p>
+          <h5 className="text-lg font-semibold ">Travel Planner</h5>
+          <small className="mb-1 italic text-yellow-500 font-mono">Plan your trips with ease and joy.</small>
         </div>
-        <div className=" text-center md:flex-row md:items-center mt-5 mb-2">
+        <div className=" text-center md:flex-row md:items-center mt-2 mb-2">
         <Link to="/#about-us" className="text-green-900 fLink px-3 py-2">About Us</Link>
 
           <a href="tel:+918976543210" className="text-green-900 fLink px-3 py-2">Contact</a>
           <Link to="/#privacy" className="text-green-900 fLink px-3 py-2">Privacy Policy</Link>
         </div>
-        <small>&copy; {currentYear} SHOBANA. All rights reserved.</small>
+        <small className='text-gray-600 font-extralight'>&copy; {currentYear} SHOBANA. All rights reserved.</small>
 
       </div>
     </footer>
